991ebaf7b984ee4525a42efb288721061859ae30
donup3 <donup08@naver.com>
2022-06-08 19:51:25 +0900

    자극 디자인 추가

--- app.py
+++ app.py
@@ -5,15 +5,39 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
 
     return render_template('tv.html')
 
 
 @app.route('/first')
-def first():  # put application's code here
+def first():
 
     return render_template('first.html')
+
+
+@app.route('/test')
+def test():
+
+    return render_template('test.html')
+
+
+@app.route('/test2')
+def test2():
+
+    return render_template('test2.html')
+
+
+@app.route('/test3')
+def test3():
+
+    return render_template('test3.html')
+
+
+@app.route('/test4')
+def test4():
+
+    return render_template('test4.html')
 
 
 if __name__ == '__main__':
